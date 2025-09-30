27c078dd36ce3f2df0df36edecae52e0a2ff7178
Misael Valles <misaell.valles@gmail.com>
2025-09-30 12:01:50 -0300

    cosas que no sirven

--- app/src/main/java/com/example/app/repository/ClienteRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.app.repository;
-
-// import com.example.app.model.Cliente;
-// import org.springframework.data.jpa.repository.JpaRepository;
-
-// public interface ClienteRepository extends JpaRepository<Cliente, Long> {
-// }
