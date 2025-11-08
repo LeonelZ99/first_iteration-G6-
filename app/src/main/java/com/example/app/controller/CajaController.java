cb3c9e30ebb0e64a0fa2f2179c4e961077857dff
Leonel <104234656+LeonelZ99@users.noreply.github.com>
2025-11-08 08:14:55 -0300

    quito imports que no se usan

--- app/src/main/java/com/example/app/controller/CajaController.java
+++ app/src/main/java/com/example/app/controller/CajaController.java
@@ -10,10 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
-import com.example.app.service.ContratoService;
-
-import com.example.app.dto.*;
-
 @RestController
 @RequestMapping("/api/caja")
 public class CajaController {
